24349f185db107dbdd3a960465fe3a08166aafd9
ChrBank <chbl@itu.dk>
2023-04-12 11:54:09 +0200

    Update namespace YourClientName to NoStepFunction

--- ScrabbleTemplate/ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/ScrabbleTemplate/Program.fs
@@ -49,11 +49,14 @@ let main argv =
         None
         
     // Uncomment this line to call your client
-    // let players    = [("Your name here", YourClientName.Scrabble.startGame)]
+    //let players    = [("Oh my Step-function", NoStepFunction.Scrabble.startGame)]
     let (dictionary, time) =
         time (fun () -> ScrabbleUtil.Dictionary.mkDict words dictAPI)
 
-    let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 2
+    //let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 2
+    
+    //last 
+    let players = spawnMultiples "NoStepFunction" dictionary NoStepFunction.Scrabble.startGame 2
 
 
     do ScrabbleServer.Comm.startGame 
